470980d38c287af75b2e4ab90c387bb389a981ca
acar12 <39702495+acar12@users.noreply.github.com>
2023-08-09 19:22:45 -0500

    schema made

---
+++ jokes_api/schema.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class JokeBase(SQLModel):
+    name: str
+    joke: str
+
+# schema 
+
+class Joke(JokeBase, table=True):
+    # field definitions for ease of use
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime | None = Field(default=datetime.utcnow(), nullable=False)
+
+class JokeCreate(JokeBase):
+    pass
+
+class JokeRead(JokeBase):
+    id: int
+    created_at: datetime
